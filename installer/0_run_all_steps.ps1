1a0e5c453799eacb0e5531155d54bfb834573a26
ccsmart <michael@gawati.org>
2018-01-23 13:59:49 +0100

    Stop using csv as source list

--- installer/0_run_all_steps.ps1
+++ installer/0_run_all_steps.ps1
@@ -1,24 +1,14 @@
-#Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
-
-$CSV = (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/gawati/setup-scripts/master/gawati/citools/scripts.csv')
-$Files = $CSV | ConvertFrom-Csv -Delim ','
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
 
 $ScriptRoot = $PSScriptRoot + '\'
-New-Item -ItemType Directory -Force -Path $ScriptRoot
 
-$Files | ForEach {
-  $URL = $_.URL
-  $File = $PSScriptRoot + '\' + $_.Filename
-  Invoke-WebRequest -Uri "$URL" -OutFile "$File"
-  }
-
-$Tasks = "setup_tools.ps1"
+$Tasks = "1_install_requirements.ps1"
 $Tasks | ForEach {
   $Script = $ScriptRoot + $_
   Start-Process "$psHome\powershell.exe" -wait -verb runas -ArgumentList "-noprofile -file ""$Script"""
   }
 
-$Tasks = "deploy_gawati_os.ps1", "gawati_devsetup.ps1"
+$Tasks = "2_deploy_minimal_VM.ps1", "3_cloneVM_installGAWATI.ps1"
 $Tasks | ForEach {
   $Script = $ScriptRoot + $_
   Start-Process "$psHome\powershell.exe" -wait -ArgumentList "-noprofile -file ""$Script"""
